e94692288593b66b0fe80d932102747d6abba539
Adeel Naeem <adeeln454@gmail.com>
2026-07-17 23:25:33 -0700

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ const SOCIAL_LINKS = [
   },
   {
     label: "Telegram",
-    href: "https://t.me/hudhudedu",
+    href: "https://t.me/W3905020",
     icon: (
       <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
         <path d="m22 2-7 20-4-9-9-4Z" />
